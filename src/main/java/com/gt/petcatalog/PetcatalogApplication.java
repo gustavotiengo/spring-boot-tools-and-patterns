3964e6f5a8d08d97109752292dc0374490cbab80
Gustavo Tiengo <gustavotiengo@gmail.com>
2024-02-05 23:09:13 -0300

    jooq and jdbc working

--- src/main/java/com/gt/petcatalog/PetcatalogApplication.java
+++ src/main/java/com/gt/petcatalog/PetcatalogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PetcatalogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetcatalogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetcatalogApplication.class, args);
+    }
 
 }
--- src/main/java/com/gt/petcatalog/controller/PetController.java
+++ src/main/java/com/gt/petcatalog/controller/PetController.java
@@ -1,15 +1,23 @@
 package com.gt.petcatalog.controller;
 
+import com.gt.petcatalog.tables.Users;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PetController {
 
+    @Autowired
+    private DSLContext create;
+
     @GetMapping("/pets")
     private ResponseEntity<String> findById() {
+        Result<Record> result = create.select().from(Users.USERS).fetch();
         return ResponseEntity.ok("pets");
     }
 }
